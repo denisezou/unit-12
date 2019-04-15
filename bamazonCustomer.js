94a86af482a6330aa384b81f899a2d64e86dfb3d
DenZou <dzou@u.northwestern.edu>
2019-04-14 22:39:32 -0500

    the inquirer prompts function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,11 @@
 //user chooses which item they'd like to buy
 //oh maybe even a selection by category and then specific items under that
 
-
+//requiring our npms
 var mysql = require("mysql");
-//var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 
-
+//creating connection with mysql
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -25,20 +25,82 @@ var connection = mysql.createConnection({
 
 });
 
+//connect mysql if no error
 connection.connect(function (err) {
     if (err) throw err;
-    else 
-    console.log("Connected with database")
+    else
+        console.log("Connected with database")
 });
 
-connection.query ('SELECT * from products', function(err, rows, fields){
-    if(!err)
-    console.log(rows);
-    else
-    console.log('error')
+//will either begin asking questions if no error, or console log that there's an error
+connection.query('SELECT * from products', function (err, rows, fields) {
+    if (!err) {
+        console.log(rows)
+        promptCustomer()
+    }
+    else {
+        console.log('error')
+    }
 });
+
+//using inquirer to ask the user questions about their purchase
+var questions = [
+    {
+        type: 'input',
+        name: 'whatId',
+        message: 'What is the ID of the item you want to buy?',
+        validate: function (value) {
+            var pass = value.match(/[0-9]/)
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a valid ID number, emphasis on number';
+
+        }
+
+    },
+
+    {
+        type: 'input',
+        name: 'howMany',
+        message: '...And how many of them?',
+        validate: function (value) {
+            var pass = value.match(/[0-9]/)
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a quantity, (hint: a number)';
+
+        }
+    }];
+
+
+
+function promptCustomer() {
+    inquirer.prompt(questions).then(function (answers) {
+        console.log(JSON.stringify(answers, null, ' '));
+    });
+
+}
+
+function checkStock() {
+    console.log('Let me look in the back... 1 sec')
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [{
+            stock_quantity:
+},
+        {
+            item_id: 
+}],
+
+    )
+
+}
 
 connection.end();
+
+
 
 
 /*
